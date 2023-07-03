eea796928e88fd68541dedb757627ccc0b23d7d5
mechaneyes <ray@mechaneyes.com>
2023-07-03 08:57:25 -0700

    api endpoint for dev server + example styling
    + have setup dev for the api app
    + updated app/page.js to use this new endpoint
    + endpoint goes from /prompt -> /prose
    + styled the example prompts a bit. people yesterday didn't see
    that they're links available for clickiness

--- app/page.js
+++ app/page.js
@@ -118,7 +118,8 @@ export default function Home() {
 
     axios
       // .post("http://127.0.0.1:5000/prompt", newPrompt, {
-      .post("https://third-eyes-flask.vercel.app/prompt", newPrompt, {
+      .post("https://thirdeyes-flask-dev.vercel.app/prose", newPrompt, {
+        // .post("https://third-eyes-flask.vercel.app/prompt", newPrompt, {
         timeout: 90000,
         headers: {
           "Content-Type": "application/json",
