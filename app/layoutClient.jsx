ca74c0c61873d6bd04306341a6e8db65856dd614
mechaneyes <ray@mechaneyes.com>
2023-07-10 16:18:17 -0700

    moved <Head> outside <ThemeProvider />

--- app/layoutClient.jsx
+++ app/layoutClient.jsx
@@ -36,26 +36,29 @@ export const metadata = {
 export default function LayoutClient({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="title" content="Third Eyes" />
+        <meta
+          name="description"
+          content="Interrogate Pitchfork Reviews via GPT-3"
+        />
+        <meta
+          name="keywords"
+          content="mechaneyes, ai, generative ai, gpt, openai, stability.ai"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Ray Weitzenberg" />
+        <meta property="og:title" content="Third Eyes" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://hearincolor.com" />
+        <meta
+          property="og:image"
+          content="https://hearincolor.com/_next/image?url=%2Fthird-eyes-og.jpg&w=1200&q=75"
+        />
+      </Head>
       <ThemeProvider theme={thirdEyesTheme}>
-        <Head>
-          <meta name="title" content="Third Eyes" />
-          <meta
-            name="description"
-            content="Interrogate Pitchfork Reviews via GPT-3"
-          />
-          <meta name="keywords" content="mechaneyes, ai, generative ai, gpt, openai, stability.ai" />
-          <meta name="robots" content="index, follow" />
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="language" content="English" />
-          <meta name="author" content="Ray Weitzenberg" />
-          <meta property="og:title" content="Third Eyes" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://hearincolor.com" />
-          <meta
-            property="og:image"
-            content="https://hearincolor.com/_next/image?url=%2Fthird-eyes-og.jpg&w=1200&q=75"
-          />
-        </Head>
         <body className={inter.className}>{children}</body>
       </ThemeProvider>
       <Analytics />
