53da0ca6af93132ad05c74d1e0b632f2e342029d
mechaneyes <ray@mechaneyes.com>
2023-07-10 18:02:42 -0700

    layout.tsx + dropped layoutClient.jsx
    + stripping out hetfield with this new branch
    + not sure if i should be using 2 separate branches, or 2 separate repos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,43 @@
-"use client";
+import { Analytics } from '@vercel/analytics/react';
+import { Inter } from 'next/font/google'
 
-import LayoutClient from "./layoutClient.jsx";
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+// https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+export const metadata = {
+  title: 'Third Eyes',
+  description: 'Interrogate Pitchfork Reviews via GPT-3.5',
+  url: 'https://hearincolor.com',
+  siteName: 'Third Eyes',
+  openGraph: {
+    title: 'Third Eyes',
+    description: 'Interrogate Pitchfork Reviews via GPT-3.5',
+    url: 'https://hearincolor.com',
+    siteName: 'Third Eyes',
+    images: [
+      {
+        url: 'https://hearincolor.com/_next/image?url=%2Fthird-eyes-og.jpg&w=1200&q=75',
+        width: 1200,
+        height: 630,
+        alt: 'ThirdEyes via HearInColor',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  return <LayoutClient>{children}</LayoutClient>;
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+      <Analytics />
+    </html>
+  )
 }
