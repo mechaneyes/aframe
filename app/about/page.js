d54d40a878da6ca34d86198854792719437c39b0
mechaneyes <ray@mechaneyes.com>
2023-08-05 13:25:47 -0400

    copy edits: about page & example prompt
    + elia suggested that conde nast is very litigious. i've tweaked the
    about page copy to suggest that i'm sticking with older data, and that
    aframe is an experiment in the hopes they won't freak out
    + also changed the example prompt from "20 techno song playlist" to "inspired
    by the loft playlist"

--- app/about/page.js
+++ app/about/page.js
@@ -12,37 +12,28 @@ export default function Page() {
         <div className="introduction__description">
           <p>
             Hello! I&apos;m Ray Weitzenberg and welcome to Aframe - an
-            exploration into the intersection of AI and music.
+            experiment at the intersection of AI and music.
           </p>
           <p>
-            Although I don&apos;t officially wear the &apos;data scientist&apos;
-            badge yet, the experience I&apos;ve gained during the creation of
-            Aframe has proven to be invaluable. Currently, I&apos;m navigating
-            the vast Python landscape with an eager mind set to venture further
-            into data science once I have mastered this programming language.
-          </p>
-          <p>
-            Aframe&apos;s content is derived from an initial{" "}
+            Aframe&apos;s content is derived from a dataset on Kaggle containing{" "}
             <a href="https://www.kaggle.com/datasets/nolanbconaway/pitchfork-data">
-              dataset of 18,393 Pitchfork reviews
-            </a>{" "}
-            , accumulated during the frosty winter of 2016-2017. Though this
-            data may not be the most recent, it provided a robust launching pad
-            for this project. I&apos;m also excited to share that the expansion
-            of our dataset is underway. This includes a variety of more
-            contemporary and diverse music data to further power Aframe.
+              18,393 Pitchfork reviews
+            </a>
+            {" "}assembled during the frosty winter of 2016-2017. While the data
+            may not be the most current, it's provided a solid foundation for my
+            venture into music-related data and my ongoing exploration of
+            generative AI.
           </p>
           <p>
             For the heavy lifting, I&apos;ve utilized state-of-the-art AI
             technologies such as OpenAI&apos;s GPT-4, GPT-3.5 and Embeddings
-            models, Langchain, the Stability.ai API, and Pinecone. In terms of
-            web development, I&apos;ve leveraged Next.js and Material Design.
+            models, Langchain, the Stability.ai API, and Pinecone.
           </p>
           <p>
             I warmly invite your feedback and comments. You can reach me at
             {"  "}
-            <a href="mailto:ray@mechaneyes.com">ray@mechaneyes.com</a>. Join me
-            in combining AI and music data in fresh, creative ways.
+            <a href="mailto:ray@mechaneyes.com">ray@mechaneyes.com</a>. Let's
+            collaborate to merge AI and music data in innovative ways.
           </p>
         </div>
       </section>
